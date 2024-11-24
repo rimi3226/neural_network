3558db8f8968b95e08c7dcc9f8f45a13306f2d13
Rimi <lora3226@inu.ac.kr>
2024-11-24 23:47:15 +0900

    Add : Node.py

--- src/class/Node.py
+++ src/class/Node.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Node:
     def __init__(self,activation):
         self.val=0                  # 노드 값
@@ -7,4 +9,16 @@ class Node:
         self.activation=activation  # 활성화함수 종류
         self.bias=[]                # 편향
         
+    # 가중치 초기화
+    def init_weight(self,next_layer_size,weight_init="He"):
+        # 1. 초기값 설정
+        if weight_init.lower() in ('he'):
+            scale = np.sqrt(2.0 / next_layer_size) 
+        elif weight_init.lower() in ('xavier'):
+            scale = np.sqrt(1.0 / next_layer_size) 
+        else:
+            scale = float(weight_init) 
         
+        # 2. 가중치 및 편향 초기화
+        self.weight = scale * np.random.randn(next_layer_size)
+        self.bias = 0        
