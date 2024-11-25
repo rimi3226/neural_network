7306fef4e86b15397ddba57000329979d17b9316
Rimi <lora3226@inu.ac.kr>
2024-11-25 14:01:55 +0900

    Add: 역전파

--- src/class/Layer.py
+++ src/class/Layer.py
@@ -8,55 +8,86 @@ import numpy as np
 from Node import Node 
 
 class Layer:
-    def __init__(self,layer_size,activation):
+    def __init__(self,layer_size,activation,next_layer_size=0,learning_rate=0.1):
         self.layer_size=layer_size                      # 레이어 크기
         self.nodes=[Node(activation) for _ in range(layer_size)]  # 레이어에 할당된 노드 리스트
         self.activation=activation                      # 해당 레이어의 활성화 함수
+        self.next_layer_size=next_layer_size;
+        self.learning_rate=learning_rate
         
-    # 노드 값 할당 -입력층
+    # 노드 값 할당 - 입력층
     def setLayer(self,input_data):
         for i in range(self.layer_size):
             self.nodes[i].val = input_data[i]
             
-            
+    # 가중치 할당 
     def setWeight(self,weight_init="He"):
         for i in range(self.layer_size):
-            self.nodes[i].init_weight(2,weight_init)
+            self.nodes[i].init_weight(self.next_layer_size,weight_init)
             print("weight: ",self.nodes[i].weight)
         
     
     # 순전파 계산
-    def forward(self):
-        output_val = []
-        output_gradient = []
-    
-        # 노드의 가중치와 값의 행렬곱 계산 및 편향 추가
-        prev_val = np.array([node.val for node in self.nodes])
+    def forward(self):    
+        # 1. 노드의 가중치와 값의 행렬곱 계산 및 편향 추가
+        val = np.array([node.val for node in self.nodes])
         weights = np.array([node.weight for node in self.nodes])
-        multiplication_val = np.round(np.dot(prev_val,weights),8)
+        multiplication_val = np.round(np.dot(val,weights),8)
 
-        print("multi : ",multiplication_val)
-
-        # 활성화 함수 적용 및 그레디언트 계산
+        # 2. 활성화 함수 적용 및 그레디언트 계산
         if self.activation == "sigmoid":
-            val = af.sigmoid(multiplication_val)
+            activation_val = af.sigmoid(multiplication_val)
             gradient = af.sigmoid_grad(multiplication_val)
         elif self.activation == "relu":
-            val = af.relu(multiplication_val)
+            activation_val = af.relu(multiplication_val)
             gradient = af.relu_grad(multiplication_val)
         elif self.activation == "leaky_relu":
-            val = af.leaky_relu(multiplication_val)
+            activation_val = af.leaky_relu(multiplication_val)
             gradient = af.leaky_relu_grad(multiplication_val) 
         elif self.activation == "tanh":
-            val = af.tanh(multiplication_val)
-            gradient = af.tanh_grad(multiplication_val)  # tanh의 도함수
+            activation_val = af.tanh(multiplication_val)
+            gradient = af.tanh_grad(multiplication_val)  
             
-        # 다음 레이어로 전달될 값 리스트에 추가
-        output_val.append(val)
-        output_gradient.append(gradient)
-        return output_val, output_gradient
+        return activation_val, gradient
         
+    # 출력층 역전파 계산
+    def get_gradient_output(self,target):
+        # 1. MSE 사용해서 손실함수 구하기
+        output=np.array([node.val for node in self.nodes])
+        gradient=(output-target)
+        
+        # 2. 각 노드의 gradient값 구하기
+        for i,node in enumerate(self.nodes):
+            node.gradient=np.round(gradient[i]*node.activation_gradient,8)
+            # print(i," gradient: ",node.gradient)
+            # print(i," activation_gradient: ",node.activation_gradient)
+            
     # 역전파 계산
-    def backward(self):
-        print()
+    def get_gradient(self,next_layer_nodes):
+        weights=np.transpose(np.array([node.weight for node in self.nodes]))
+        res_multiplication=np.dot(next_layer_nodes,weights)
+        # print("weights: ",weights)
+        # print("next_layer_node * weight transpose : ",res_multiplication)
+        
+        for i,node in enumerate(self.nodes):
+            node.gradient=np.round(res_multiplication[i]*node.activation_gradient,15)
+            # print("grad : ",node.gradient)
+            # print("activation_grad: ",node.activation_gradient)
+            
+    # 가중치 갱신
+    def update_weight(self,prev_layer_val):
+        for i,node in enumerate(self.nodes):
+            for j,weight in enumerate(node.weight):
+                # print("------------------------------------")
+                # print(weight)
+                weight-=self.learning_rate*node.gradient*prev_layer_val[j]
+                self.nodes[i].weight[j]=weight
+                # print("* result : ",self.learning_rate*node.gradient*prev_layer_val[j])
+                # print("* : ", node.gradient, " ", prev_layer_val[j])
+                # print(weight)
+        
+        
+        
+        
+
         
